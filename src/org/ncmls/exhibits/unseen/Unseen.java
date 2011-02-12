a6509da65e6dc8811066232b2249c9acbf41fe06
eatoin5hrdlu <peter.reintjes@ncmls.org>
2011-02-12 01:48:17 -0500

    Initial Readme. CAn I create the remote repository entirely from inside Eclipse?

---
+++ src/org/ncmls/exhibits/unseen/Unseen.java
@@ -0,0 +1,13 @@
+package org.ncmls.exhibits.unseen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Unseen extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
